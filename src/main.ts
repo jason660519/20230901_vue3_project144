4fc1a06e7aac4eaf01fd4b5194e337208d62788d
Jason blackdragon <jason660519@gmail.com>
2023-09-02 09:08:03 +1000

    docs: 023

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
-
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).mount('#app')
+//@ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+const app = createApp(App)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+import 'virtual:svg-icons-register'
+app.mount('#app')
+
+console.log(import.meta.env)
